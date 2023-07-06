a7502022d65d3256d28413b432904f10631bc3fe
너나들이 <tychejin@naver.com>
2023-07-06 18:00:56 +0900

    Chore: Redis Repository 설정

--- redis-sample-02/src/test/java/com/example/redis/service/EmployeeServiceTest.java
+++ redis-sample-02/src/test/java/com/example/redis/service/EmployeeServiceTest.java
@@ -130,10 +130,10 @@ class EmployeeServiceTest {
 
   @DisplayName("findAllEmployee_EmployeeDto 전체를 조회")
   @Test
-  void testFindAllEmployee() throws Exception {
+  void testFindAllEmployee() {
 
     // Given
-    saveEmployeeSample();
+    saveAllEmployees();
 
     // When
     employeeService.findAllEmployee();
@@ -143,12 +143,12 @@ class EmployeeServiceTest {
 
   @DisplayName("findAllByIdEmployee_ID를 기준으로 EmployeeDto 전체를 조회")
   @Test
-  void testFindAllByIdEmployee() throws Exception {
+  void testFindAllByIdEmployee() {
 
     // Given
-    saveEmployeeSample();
+    saveAllEmployees();
 
-    List<String> ids = Arrays.asList("E0001", "E0002", "E0003");
+    List<String> ids = Arrays.asList("E0001", "E0003", "E0005");
 
     // When
     employeeService.findAllByIdEmployee(ids);
@@ -162,27 +162,80 @@ class EmployeeServiceTest {
   }
 
   @Disabled
-  void saveEmployeeSample() {
+  void saveAllEmployees() {
 
-    for (int a = 0; a < 10; a++) {
+    List<EmployeeDto> employees = new ArrayList<>();
 
-      List<EmployeeDto.Department> departments = new ArrayList<>();
+    employees.add(
+        EmployeeDto.builder()
+            .id("E0001")
+            .name("세종대왕")
+            .gender("남성")
+            .age(30)
+            .hireDate(LocalDate.of(2022, 1, 15))
+            .departments(createSampleDepartments(LocalDate.of(2022, 1, 15), "마케팅", "영업"))
+            .build());
+
+    employees.add(
+        EmployeeDto.builder()
+            .id("E0002")
+            .name("유관순")
+            .gender("여성")
+            .age(35)
+            .hireDate(LocalDate.of(2021, 7, 10))
+            .departments(createSampleDepartments(LocalDate.of(2021, 7, 10), "연구 및 개발"))
+            .build());
+
+    employees.add(
+        EmployeeDto.builder()
+            .id("E0003")
+            .name("안중근")
+            .gender("남성")
+            .age(28)
+            .hireDate(LocalDate.of(2022, 8, 29))
+            .departments(createSampleDepartments(LocalDate.of(2023, 3, 1), "인사"))
+            .build());
+
+    employees.add(
+        EmployeeDto.builder()
+            .id("E0004")
+            .name("신사임당")
+            .gender("여성")
+            .age(32)
+            .hireDate(LocalDate.of(2020, 9, 8))
+            .departments(createSampleDepartments(LocalDate.of(2020, 9, 8), "영업", "마케팅"))
+            .build());
+
+    employees.add(
+        EmployeeDto.builder()
+            .id("E0005")
+            .name("윤동주")
+            .gender("남성")
+            .age(29)
+            .hireDate(LocalDate.of(2022, 5, 12))
+            .departments(createSampleDepartments(LocalDate.of(2022, 5, 12), "연구 및 개발"))
+            .build());
+
+    employeeRepository.saveAll(employees);
+  }
+
+  List<EmployeeDto.Department> createSampleDepartments(
+      LocalDate localDate,
+      String... departmentNames) {
+
+    List<EmployeeDto.Department> departments = new ArrayList<>();
+    LocalDate transferDate = localDate;
+
+    for (String departmentName : departmentNames) {
       departments.add(
           EmployeeDto.Department.builder()
-              .department("인사")
-              .transferDate(LocalDate.of(2022, 5, 2))
+              .department(departmentName)
+              .transferDate(transferDate)
               .build());
 
-      EmployeeDto employee = EmployeeDto.builder()
-          .id("E" + String.format("%04d", a))
-          .name("name_" + a)
-          .gender("남")
-          .age(45)
-          .hireDate(LocalDate.of(2022, 5, 2))
-          .departments(departments)
-          .build();
-
-      employeeRepository.save(employee);
+      transferDate = transferDate.plusMonths(6);
     }
+
+    return departments;
   }
 }
